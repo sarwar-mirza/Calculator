761cee2ef5cbf1dac99b7a2245e9b0ccbc719af2
mithu <nilchirkutpoison@gmail.com>
2024-02-21 18:55:29 +0600

    Calculator using Javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+var string = "";
+var buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach((item)=>{
+    item.addEventListener('click', (e)=>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            document.querySelector('input').value = string;
+        }
+
+        else if(e.target.innerHTML == 'C'){
+            string = "";
+            document.querySelector('input').value = string;
+        }
+
+        else if(e.target.innerHTML == 'D'){
+            string = string.substring(0, string.length-1);
+            document.querySelector('input').value = string;
+        }
+        else{
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            document.querySelector('input').value = string;
+        }
+        
+    })
+})
